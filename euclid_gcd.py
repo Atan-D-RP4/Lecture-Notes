40f25a66fdfaa19e74238cf6890be358f6ab835c
Dubakula Sai Venkata Chaitanya <chaitanyarp4@gmail.com>
2024-11-18 12:03:54 +0530

    Update euclid_gcd.py

--- euclid_gcd.py
+++ euclid_gcd.py
@@ -12,3 +12,23 @@ def gcdExtended(a, b):
     y = x1
 
     return gcd, x, y
+
+def findMinimalSolution(a, b):
+    # Get initial solution
+    gcd, x, y = gcdExtended(abs(a), abs(b))
+    
+    # Handle negative inputs
+    if a < 0: x = -x
+    if b < 0: y = -y
+    
+    # Make sure x ≤ y
+    # We can add or subtract b/gcd to x and a/gcd to y to get new solutions
+    k = (y - x) // 2
+    x += (b//gcd) * k
+    y -= (a//gcd) * k
+    
+    # If still x > y, swap and negate both
+    if x > y:
+        x, y = -y, -x
+    
+    return x, y, gcd
