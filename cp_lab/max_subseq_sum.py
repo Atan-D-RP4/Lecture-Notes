6d9da775c153250cc561dfba1cf9dbd6746a5522
Dubakula Sai Venkata Chaitanya <chaitanyarp4@gmail.com>
2024-12-07 07:25:47 +0530

    Update max_subseq_sum.py

--- cp_lab/max_subseq_sum.py
+++ cp_lab/max_subseq_sum.py
@@ -1,12 +1,7 @@
-impl Solution {
-    #[inline]
-    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
-        let mut mc = nums[0];
-        let mut mt = nums[0];
-        for i in nums.iter().skip(1) {
-            mc = if mc < 0 { *i } else { *i + mc };
-            mt = mc.max(mt);
-        }
-        mt
-    }
-}
+def max_sub_array(nums):
+    mc = nums[0]
+    mt = nums[0]
+    for i in nums[1:]:
+        mc = i if mc < 0 else i + mc
+        mt = max(mc, mt)
+    return mt
